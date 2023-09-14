cf1ae917b2217b9b2a44c767710ff1329b51a1a5
Niroasd <nico.roininen@edu.turkuamk.fi>
2023-09-14 09:22:03 +0300

    Starting task 11 from ch01

---
+++ Ch01/combineLists.py
@@ -0,0 +1,20 @@
+def main():
+
+    list1 = [1, 3, 5, 7, 9]
+    list2 = [0, 2, 4, 6, 8]
+
+    combine_lists(list1, list2)
+
+def combine_lists(list1: list, list2: list) -> list:
+    combinedList = []
+    list = list1
+    list2 = list2
+
+    while(True):
+        pass
+        
+
+    return combinedList
+
+
+main()
