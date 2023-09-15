e569494ab6cbf203828e89fa2c0095072d4ed465
Niroasd <123634396+Niroasd@users.noreply.github.com>
2023-09-16 02:47:28 +0300

    fixed restaurant, i was a dumbass.

--- Ch01/restaurant.py
+++ Ch01/restaurant.py
@@ -4,16 +4,8 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f'{self.name}')
-        print(f'{self.cuisine_type}')
+        print(f'{self.name} serves wonderful {self.cuisine_type}.')
+
 
     def open_restaurant(self):
-        print(f'{self.name} serves wonderful {self.cuisine_type}.')
         print(f'{self.name} is open. Come on in!')
-
-	
-restaurant = Restaurant('Kotipizza', 'pizza')
-print(restaurant.name)
-print(restaurant.cuisine_type)
-restaurant.describe_restaurant()
-restaurant.open_restaurant()
