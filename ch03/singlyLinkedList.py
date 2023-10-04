d62b35ecfd7f0b0d35d7b7d9d9a9f795273327d0
Niroasd <nico.roininen@edu.turkuamk.fi>
2023-10-04 09:24:36 +0300

    working on week3.

--- ch03/singlyLinkedList.py
+++ ch03/singlyLinkedList.py
@@ -8,7 +8,8 @@ class ListNode():
 
 class SinglyLinkedList():
     def __init__(self):
-        self._head = None
+        self._head = self._tail = None
+        self._size = 0
 
     def __repr__(self):
         current_node = self._head
@@ -16,7 +17,11 @@ class SinglyLinkedList():
         while current_node:
             values += f', {current_node.data}'
             current_node = current_node.next
-        return f'<SinglyLinkedList: [{values.lstrip(", ")}]>'
+        plural = '' if self._size == 1 else 's'
+        return f'<SinglyLinkedList: (contains {self._size} element{plural}): [{values.lstrip(", ")}]>'
+    
+    def __len__(self):
+        return self._size
      
     def append(self, value):
         """
@@ -30,20 +35,23 @@ class SinglyLinkedList():
         # Create the node with the value
         node = ListNode(value)
         # If list is empty just point the header to the new node
-        if not self._head:
-            self._head = node
+        if self._tail is None:
+            self._head = self._tail = node
         else:
             # if list is not empty, search for the last element and point it to the new node
-            current_node = self._head
-            while current_node.next != None:
-                current_node = current_node.next
-            current_node.next = node
+            self._tail.next = node
+            self._tail = node
+
+        self._size += 1
             
     def pop(self):
 
+        #if empty return none
         if self._head is None:
             return None
         
+
+        #removing a single element list
         if self._head.next is None:
             val = self._head.data
             del self._head
@@ -52,19 +60,25 @@ class SinglyLinkedList():
 
         
         current_node = self._head
+        val = self._tail.data
+        
+        valueToDelete = self._tail
+        del(valueToDelete)
 
         while current_node.next != None:
             secondLast = current_node
             current_node = current_node.next
 
-        val = current_node.data
-        del(current_node)
-        secondLast.next = None
+
+        
+        self._tail = secondLast
 
         return val
 
 list = SinglyLinkedList()
-for i in 'abc':
+for i in 'abcfkgdgm':
     list.append(i)
+
+print(list._tail.data)
 val = list.pop()
 print(val, list)
